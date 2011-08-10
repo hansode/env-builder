0589f1f75e3c636a269ca624a1723ba451423a9c
Masahito Yoshida <masahito@axsh.net>
2011-08-10 14:47:14 +0900

    Add linux-headers-x.x.x

--- ubuntu/10.04/zfs/01_setup-base.sh
+++ ubuntu/10.04/zfs/01_setup-base.sh
@@ -7,11 +7,18 @@ set -e
 
 deb_pkgs="
  python-software-properties/natty
+ dkms
+ spl
+ zlib1g-dev uuid-dev libblkid-dev libselinux-dev parted lsscsi
 "
 
 (cd ../ubuntu-natty/ && make)
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ${deb_pkgs}
 
 sudo add-apt-repository ppa:dajhorn/zfs
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install linux-headers-`uname -r`
+
+#sudo DEBIAN_FRONTEND=noninteractive apt-get -y install zfs-lib spl dkms
+#sudo DEBIAN_FRONTEND=noninteractive apt-get -y install spl-dkms
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install zfs
