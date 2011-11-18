c987965f4e33124e4ee9f8eb9655c7782092fd60
Masahito Yoshida <masahito@axsh.net>
2011-11-18 21:55:07 +0900

    lxc.spec cannot use fakeroot.

--- rhel/6/lxc/03_install-rpm.sh
+++ rhel/6/lxc/03_install-rpm.sh
@@ -16,6 +16,7 @@ local_rpm_pkg_paths="
 "
 
 for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
+  echo ${local_rpm_pkg_path}
   local_rpm_pkg=$(basename ${local_rpm_pkg_path})
   rpm -ql ${local_rpm_pkg%%.rpm} >/dev/null || {
     rpm -ivh ${local_rpm_pkg_path}
