9e088253804ab1455dc4296e831da8c90a694826
Masahito Yoshida <masahito@axsh.net>
2011-11-18 21:24:09 +0900

    do not depend on root permission.

--- rhel/6/drbd/03_install-rpm.sh
+++ rhel/6/drbd/03_install-rpm.sh
@@ -13,8 +13,9 @@ local_rpm_pkg_paths="
 
 for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
   local_rpm_pkg=$(basename ${local_rpm_pkg_path})
+  echo ${local_rpm_pkg_path}
   rpm -ql ${local_rpm_pkg%%.rpm} >/dev/null || {
-    rpm -ivh ${local_rpm_pkg_path}
+    sudo rpm -ivh ${local_rpm_pkg_path}
   }
 done
 
