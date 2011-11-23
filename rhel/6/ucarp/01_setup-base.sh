af93826dc9e328e82bf2520d63d74776367e9a06
Masahito Yoshida <masahito@axsh.net>
2011-11-23 11:53:26 +0900

    use sudo.

--- rhel/6/ucarp/01_setup-base.sh
+++ rhel/6/ucarp/01_setup-base.sh
@@ -5,14 +5,8 @@
 
 set -e
 
-rpm_pkgs="
- ucarp
-"
-
-(cd ../epel-release && make)
-yum install -y ${rpm_pkgs}
-
-chkconfig        ucarp on
+chkconfig --list ucarp
+sudo chkconfig ucarp on
 chkconfig --list ucarp
 
 exit 0
