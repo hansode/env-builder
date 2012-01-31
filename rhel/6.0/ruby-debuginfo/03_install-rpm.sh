2d7bdcb5aef6750130e69d46ee64baeb9be5658b
Masahito Yoshida <masahito@axsh.net>
2012-01-31 23:38:37 +0900

    use force installation.

--- rhel/6.0/ruby-debuginfo/03_install-rpm.sh
+++ rhel/6.0/ruby-debuginfo/03_install-rpm.sh
@@ -6,13 +6,24 @@ arch=*86*
 uname_r=$(uname -r | sed 's,-,_,g')
 
 local_rpm_pkg_paths="
-  ${rpmbuild_dir}/RPMS/${arch}/ruby-debuginfo-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-debuginfo-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-devel-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-docs-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-irb-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-libs-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-rdoc-${ruby_version}.el6.${arch}.rpm
+ ${rpmbuild_dir}/RPMS/${arch}/ruby-ri-${ruby_version}.el6.${arch}.rpm
 "
 
-for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
-  local_rpm_pkg=$(basename ${local_rpm_pkg_path})
-  echo ${local_rpm_pkg_path}
-  rpm -ql ${local_rpm_pkg%%.rpm} >/dev/null || {
-    sudo rpm -ivh ${local_rpm_pkg_path}
-  }
-done
+#for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
+#  local_rpm_pkg=$(basename ${local_rpm_pkg_path})
+#  echo ${local_rpm_pkg_path}
+#  rpm -ql ${local_rpm_pkg%%.rpm} >/dev/null && {
+#    sudo rpm -Uvh --force ${local_rpm_pkg_path}
+#  } || {
+#    sudo rpm -ivh ${local_rpm_pkg_path}
+#  }
+#done
+
+sudo rpm -Uvh --force ${local_rpm_pkg_paths}
