52fa926fc09c493a9a07d829e71e5bdd9ca9e985
Masahito Yoshida <masahito@axsh.net>
2012-01-31 23:38:06 +0900

    change rpmbuild option.

--- rhel/6.0/ruby-debuginfo/02_build-rpm.sh
+++ rhel/6.0/ruby-debuginfo/02_build-rpm.sh
@@ -4,5 +4,6 @@
 
 #rpmbuild --rebuild ./ruby-${ruby_version}.el6.src.rpm
 
-rpm -ivh ./ruby-${ruby_version}.el6.src.rpm
-rpmbuild -ba ${rpmbuild_dir}/SPECS/ruby.spec
+#rpm -ivh ./ruby-${ruby_version}.el6.src.rpm
+#rpmbuild -ba --define="optflags -pg -g3 -O0" ${rpmbuild_dir}/SPECS/ruby.spec
+rpmbuild --rebuild --define="optflags -pg -g3 -O0" ./ruby-${ruby_version}.el6.src.rpm
