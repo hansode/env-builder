c33e98cae7ce4ddf80a59dac3f5821d401a965b0
Masahito Yoshida <masahito@axsh.net>
2012-02-20 01:58:32 +0900

    add alias to .bashrc

--- cygwin/1.7/apt-cyg/01_setup-base.sh
+++ cygwin/1.7/apt-cyg/01_setup-base.sh
@@ -3,8 +3,13 @@
 #
 set -e
 
-[ -x /usr/bin/apt-cyg ] && exit 0
+[ -x /usr/bin/apt-cyg ] || {
+  wget http://apt-cyg.googlecode.com/svn/trunk/apt-cyg
+  mv apt-cyg /usr/bin
+  chmod +x /usr/bin/apt-cyg
+}
 
-wget http://apt-cyg.googlecode.com/svn/trunk/apt-cyg
-mv apt-cyg /usr/bin
-chmod +x /usr/bin/apt-cyg
+alias_aptcyg="alias apt-cyg='apt-cyg -u'"
+egrep -q "^${alias_aptcyg}" ~/.bashrc || {
+  echo ${alias_aptcyg} >> ~/.bashrc
+}
