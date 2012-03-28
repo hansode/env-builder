f7db1647c2c672cf89a4455d5e9db83179b617fe
Masahito Yoshida <masahito@axsh.net>
2012-03-29 03:36:17 +0900

    execute postinstall script of gcc family.

--- cygwin/1.7/00core-devel/00_prepare-pkg.sh
+++ cygwin/1.7/00core-devel/00_prepare-pkg.sh
@@ -19,4 +19,10 @@ for pkg in ${pkgs}; do
   apt-cyg show | grep -q ${pkg} >/dev/null || apt-cyg install ${pkg}
 done
 
+# /usr/bin/apt-cyg: line 402: /etc/postinstall/gcc.sh: Permission denied
+# /usr/bin/apt-cyg: line 402: /etc/postinstall/gcc-g++.sh: Permission denied
+
+[ -f /etc/postinstall/gcc.sh.done ] && bash /etc/postinstall/gcc.sh.done
+[ -f /etc/postinstall/gcc-g++.sh.done ] && bash /etc/postinstall/gcc-g++.sh.done
+
 exit 0
