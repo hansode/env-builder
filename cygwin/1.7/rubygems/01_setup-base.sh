a92840e1e32ffbcd2b56b796a7547df82d276963
Masahito Yoshida <masahito@axsh.net>
2012-03-31 02:04:43 +0900

    fix directory to change.

--- cygwin/1.7/rubygems/01_setup-base.sh
+++ cygwin/1.7/rubygems/01_setup-base.sh
@@ -5,6 +5,6 @@
 [ -x /usr/bin/gem ] || {
   [ -f rubygems-${rubygems_ver}.tgz ] || wget http://production.cf.rubygems.org/rubygems/rubygems-${rubygems_ver}.tgz
   tar zxvf rubygems-${rubygems_ver}.tgz
-  cd rubygems-${rubygems_ver}.tgz
+  cd rubygems-${rubygems_ver}
   ruby ./setup.rb
 }
