42a3473ad19a253ce08baebc11b4b33fa945a2b4
Masahito Yoshida <masahito@axsh.net>
2012-05-07 20:39:52 +0900

    add dracut-kernel to dependency packages.

--- rhel/6/openvswitch-kmod/00_prepare-pkg.sh
+++ rhel/6/openvswitch-kmod/00_prepare-pkg.sh
@@ -7,8 +7,10 @@ pkgs="
  gcc
  openssl-devel
  redhat-rpm-config
+ dracut-kernel
 "
 # The macro "%kernel_module_package_buildreqs" is defined in the file "/usr/lib/rpm/redhat/macros" from package redhat-rpm-config, 
+# when installing kmod-openvswitch, dracut-kernel is needed.
 
 for pkg in ${pkgs}; do
   rpm -ql ${pkg} >/dev/null || sudo yum install -y ${pkg}
