663d765d1025db48589f45cec31251b4dd2aa88a
Masahito Yoshida <masahito@axsh.net>
2012-05-07 20:30:23 +0900

    add dependency packages.

--- rhel/6/openvswitch-kmod/00_prepare-pkg.sh
+++ rhel/6/openvswitch-kmod/00_prepare-pkg.sh
@@ -4,7 +4,11 @@ set -e
 
 pkgs="
  kernel-devel
+ gcc
+ openssl-devel
+ redhat-rpm-config
 "
+# The macro "%kernel_module_package_buildreqs" is defined in the file "/usr/lib/rpm/redhat/macros" from package redhat-rpm-config, 
 
 for pkg in ${pkgs}; do
   rpm -ql ${pkg} >/dev/null || sudo yum install -y ${pkg}
