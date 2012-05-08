8da896c97241739f76917994b4c5a2b9808ec59a
Masahito Yoshida <masahito@axsh.net>
2012-05-08 19:57:30 +0900

    optomize "build" task.

--- rhel/6/openvswitch/02_build-rpm.sh
+++ rhel/6/openvswitch/02_build-rpm.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 set -e
+set -x
 
 . ./_common.sh
 
 [ -d openvswitch-${openvswitch_version} ] || exit 1
 
-rsync -a ./openvswitch-${openvswitch_version}.tar.gz $HOME/rpmbuild/SOURCES/
+[ -f $HOME/rpmbuild/SOURCES/openvswitch-${openvswitch_version}.tar.gz ] || {
+  rsync -a ./openvswitch-${openvswitch_version}.tar.gz $HOME/rpmbuild/SOURCES/
+}
 
 cd openvswitch-${openvswitch_version}
-
-rpmbuild -bb rhel/openvswitch.spec
-rpmbuild -bb rhel/openvswitch-kmod-rhel6.spec
+(
+for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
+  [ -f ${local_rpm_pkg_path} ] || exit 1
+done
+) || (
+ rpmbuild -bb rhel/openvswitch.spec
+ rpmbuild -bb rhel/openvswitch-kmod-rhel6.spec
+)
