1837b53e51f82786979da4af3a861a6012dfab94
Masahito Yoshida <masahito@axsh.net>
2012-05-08 19:26:39 +0900

    optomize "build" task.

--- rhel/6/lxc/02_build-rpm.sh
+++ rhel/6/lxc/02_build-rpm.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+set -x
 
 . ./_common.sh
 
 [ -d lxc-${lxc_version} ] || exit 1
 
 cd lxc-${lxc_version}
-#[ -f Makefile ] || ./configure
-make rpm
+(
+for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
+  [ -f ${local_rpm_pkg_path} ] || exit 1
+done
+) || make rpm
--- rhel/6/lxc/03_install-rpm.sh
+++ rhel/6/lxc/03_install-rpm.sh
@@ -1,23 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 . ./_common.sh
-
-arch=$(arch)
-case ${arch} in
-*86) arch=*86 ;;
-esac
-
-uname_r=$(uname -r | sed 's,-,_,g')
-rpmbuild_dir=${HOME}/rpmbuild
-
-
-#/root/rpmbuild/RPMS/i686/lxc-0.7.5-1.i686.rpm
-#/root/rpmbuild/RPMS/i686/lxc-devel-0.7.5-1.i686.rpm
-#/root/rpmbuild/RPMS/i686/lxc-debuginfo-0.7.5-1.i686.rpm
-
-local_rpm_pkg_paths="
-  ${rpmbuild_dir}/RPMS/${arch}/lxc-${lxc_version}-1.${arch}.rpm
-"
 
 for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
   echo ${local_rpm_pkg_path}
