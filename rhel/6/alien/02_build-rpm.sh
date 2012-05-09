6f3de48d16d72bca14f53a2b96725d3554b76fda
Masahito Yoshida <masahito@axsh.net>
2012-05-09 19:30:20 +0900

    optomize "build" task of alien.

--- rhel/6/alien/02_build-rpm.sh
+++ rhel/6/alien/02_build-rpm.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 . ./_common.sh
 
-rpmbuild -ta alien_${alien_version}.tar.gz
+(
+for local_rpm_pkg_path in ${local_rpm_pkg_paths}; do
+  [ -f ${local_rpm_pkg_path} ] || exit 1
+done
+) || rpmbuild -ta alien_${alien_version}.tar.gz
