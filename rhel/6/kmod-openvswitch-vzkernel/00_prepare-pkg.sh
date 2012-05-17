3030e3b7966c085a6a53aa0f11ae76bb9b1d61c7
Masahito Yoshida <masahito@axsh.net>
2012-05-18 05:37:33 +0900

    add vzkernel-devel to dependency package.

--- rhel/6/kmod-openvswitch-vzkernel/00_prepare-pkg.sh
+++ rhel/6/kmod-openvswitch-vzkernel/00_prepare-pkg.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+(cd ../rpmbuild/ && make)
+(cd ../openvz/ && make)
+
 pkgs="
+  vzkernel-devel
 "
 
 for pkg in ${pkgs}; do
   rpm -ql ${pkg} >/dev/null || sudo yum install -y ${pkg}
 done
 
-(cd ../rpmbuild/ && make)
-(cd ../openvz/ && make)
