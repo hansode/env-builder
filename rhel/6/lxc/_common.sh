a91b98535cd065e88fa3a14eaa9d831d73be8baf
Masahito Yoshida <masahito@axsh.net>
2012-05-23 18:24:39 +0900

    try to enable to set target architecture type.

--- rhel/6/lxc/_common.sh
+++ rhel/6/lxc/_common.sh
@@ -1,11 +1,16 @@
 lxc_version=0.7.5
 
-arch=$(arch)
+arch=${arch:-$(arch)}
 case ${arch} in
-*86) arch=*86 ;;
+x86_64)
+  arch=amd64
+  ;;
+*)
+  arch=i386
+  ;;
 esac
 
-uname_r=$(uname -r | sed 's,-,_,g')
+#uname_r=$(uname -r | sed 's,-,_,g')
 rpmbuild_dir=${HOME}/rpmbuild
 
 #/root/rpmbuild/RPMS/i686/lxc-0.7.5-1.i686.rpm
