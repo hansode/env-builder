6fa2a36fd25b6bae588519dc4d368400a2f9aae1
Masahito Yoshida <masahito@axsh.net>
2012-05-24 19:08:08 +0900

    fix archtecture type for i*86.

--- rhel/6/kmod-openvswitch-vzkernel/_common.sh
+++ rhel/6/kmod-openvswitch-vzkernel/_common.sh
@@ -2,17 +2,13 @@ openvswitch_version=1.4.1
 
 arch=${arch:-$(arch)}
 case ${arch} in
-x86_64)
-  arch=x86_64
-  ;;
-*)
-  arch=i386
-  ;;
+i*86)   basearch=i386; arch=i686;;
+x86_64) basearch=${arch};;
 esac
 
 #uname_r=$(uname -r | sed 's,-,_,g')
 rpmbuild_dir=${HOME}/rpmbuild
 
 local_rpm_pkg_paths="
-  ${rpmbuild_dir}/RPMS/${arch}/kmod-openvswitch-vzkernel-${openvswitch_version}-1.el6.${arch}.rpm
+  ${rpmbuild_dir}/RPMS/${basearch}/kmod-openvswitch-vzkernel-${openvswitch_version}-1.el6.${basearch}.rpm
 "
