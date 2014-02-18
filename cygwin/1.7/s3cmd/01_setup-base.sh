5581d1ea51579e1a97dd0dba642850a4bbed95c9
Masahito Yoshida <masahito@axsh.net>
2014-02-18 12:12:30 +0900

    cygwin: fix s3cmd installation.

--- cygwin/1.7/s3cmd/01_setup-base.sh
+++ cygwin/1.7/s3cmd/01_setup-base.sh
@@ -2,10 +2,11 @@
 
 . ./_common.sh
 
-[ -f s3cmd-${s3cmd_ver}.zip ] || {
-  wget -O s3cmd-${s3cmd_ver}.zip "http://sourceforge.net/projects/s3tools/files/latest/download?source=files"
-  mv -i "download@source=files" s3cmd-${s3cmd_ver}.tar.gz
-  unzip s3cmd-${s3cmd_ver}.zip
+s3cmd_tarball=s3cmd-${s3cmd_ver}.tar.gz
+
+[ -f ${s3cmd_tarball} ] || {
+  wget -O ${s3cmd_tarball} "http://sourceforge.net/projects/s3tools/files/latest/download?source=files"
+  tar zxvf ${s3cmd_tarball}
 }
 
 extracted_dir=$(for i in s3cmd-*; do [ -d $i ] && echo $i; done | sort -r | head -1)
