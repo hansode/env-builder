c2c09c2d57c002d6e497b5777d6782354d090005
Masahito Yoshida <masahito@axsh.net>
2014-05-05 21:05:00 +0900

    remove git-flow.

--- cygwin/1.7/git-flow/00_prepare-pkg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pkgs="
- util-linux
-"
-
-for pkg in ${pkgs}; do
-  apt-cyg show | grep -q ${pkg} >/dev/null || apt-cyg install ${pkg}
-done
--- cygwin/1.7/git-flow/01_setup-base.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# . ./_common.sh
-
-which git-flow || {
-  [ -f gitflow-installer.sh ] || wget --no-check-certificate https://github.com/nvie/gitflow/raw/develop/contrib/gitflow-installer.sh
-  bash ./gitflow-installer.sh
-}
